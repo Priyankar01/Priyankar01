46b967eaf706be21384fbef05357c45efaeb3c58
Priyankar01 <priyankardutta001@gmail.com>
2023-12-24 04:43:00 +0530

    fixing footer.css error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './footer.css';
+
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
