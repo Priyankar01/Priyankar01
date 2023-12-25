f5be5f7f8779df66f3c85eec29f1c8bb58ce1133
Priyankar01 <priyankardutta001@gmail.com>
2023-12-25 22:38:09 +0530

    deleted speaking

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Experience from './components/Experience/Experience';
 import Footer from './components/Footer/Footer';
 import Landing from './components/Landing/Landing';
 import Projects from './components/Projects/Project';
-import Speaking from './components/Speaking/Speaking';
 import Navbar from './components/Navbar/Navbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
